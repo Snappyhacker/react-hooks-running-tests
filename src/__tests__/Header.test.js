476924052a756419e30d58f0ae23bbdf6460a404
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-07-31 13:10:21 +0300

    initial commit

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,9 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
 import Header from "../components/Header";
 
-test("displays the text 'hello from the Header!'", () => {
+test("renders the header text", () => {
   render(<Header />);
-
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  expect(screen.getByText("Hello from the header")).toBeInTheDocument();
 });
